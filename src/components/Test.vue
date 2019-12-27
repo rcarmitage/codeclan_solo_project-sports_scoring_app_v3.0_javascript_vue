00f659149a8a9a057e1954377e526a5f52291656
Robert Armitage <robert.c.armitage@gmail.com>
2019-12-27 02:53:58 +0000

    initial commit: HelloWorld component removed, Test component added and displays

---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Test text</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+h1 {
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
+}
+
+</style>
