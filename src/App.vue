35c9c370afdb53004b05f8e96c6250df99bd0ed2
Robert Armitage <robert.c.armitage@gmail.com>
2019-12-28 01:10:49 +0000

    added CSS, App component now looks like Homepage on Ruby app, should this be in Homepage component?

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <div id="app">
-    <Homepage msg="Homepage msg"/>
+    <!-- <title>Ultimate Hackathon League</title> -->
+    <h1>Ultimate Hackathon League</h1>
+    <h2 class="inspired-by-codeclan">Inspired by CodeClan</h2>
+    <nav class="main-nav">
+        <ul>
+            <li>
+              <a class="btn">Home</a>
+            </li>
+            <li>
+              <a class="btn">Teams</a>
+            </li>
+            <li>
+              <a class="btn">Fixtures</a>
+            </li>
+            <li>
+              <a class="btn">League Table</a>
+            </li>
+            <li>
+              <a class="btn">About</a>
+            </li>
+        </ul>
+    </nav>
+    <footer>
+        <p>&copy; 2019 Made by Rob</p>
+    </footer>
   </div>
 </template>
 
@@ -10,7 +34,7 @@ import Homepage from './components/Homepage.vue'
 export default {
   name: 'app',
   components: {
-    Test
+    Homepage
   }
 }
 </script>
@@ -18,10 +42,64 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  --primary-blue: #0c80d0;
+  --dark-blue: #005B9A;
+  --dark: #333;
+  --light-blue: #3385ff;
+  --light: #fff;
+  --shadow: 0 1px 5px rgba(104, 104, 104, 0.8);
 }
+
+html {
+  box-sizing: border-box;
+  color: var(--dark);
+}
+
+body {
+  background: #abcad6;
+  margin: 30px 50px;
+  line-height: 1.4;
+}
+
+@media screen {
+  .header-title {
+    font-size: 3rem;
+    text-align: left;
+    grid-area: header-title;
+    margin: 0;
+  }
+
+  .inspired-by-codeclan {
+    font-size: 0.8rem;
+    grid-area: inspired-by-codeclan;
+    margin: 0;
+  }
+
+  .main-nav ul {
+    display: grid;
+    grid-gap: 10px;
+    padding: 0;
+    list-style: none;
+    grid-template-columns: repeat(5, 1fr);
+  }
+  
+  .main-nav a {
+    background: var(--primary-blue);
+    display: block;
+    text-decoration: none;
+    padding: 0.3rem 0.4rem;
+    text-align: center;
+    color: var(--dark);
+    text-transform: uppercase;
+    font-size: 0.9rem;
+    box-shadow: var(--shadow);
+  }
+  
+  .main-nav a:hover {
+    background: var(--dark);
+    color: var(--primary-blue);
+  }
+
+}
+
 </style>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,27 +1,6 @@
 <template>
     <div>
-        <h1>Ultimate Hackathon League</h1>
-        <h3>Inspired by CodeClan</h3>
-        <nav>
-            <ul>
-                <li>Home</li>f
-            </ul>
-            <ul>
-                <li>Teams</li>f
-            </ul>
-            <ul>
-                <li>Fixtures</li>f
-            </ul>
-            <ul>
-                <li>League Table</li>f
-            </ul>
-            <ul>
-                <li>About</li>f
-            </ul>
-        </nav>
-        <footer>
-            <p>&copy; 2019 Made by Rob</p>
-        </footer>
+        <p>Homepage stuff goes here</p>
     </div>
 </template>
 
@@ -34,17 +13,6 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-
-:root {
-    --primary-blue: #0c80d0;
-    --dark-blue: #005B9A;
-    --dark: #333;
-    --light-blue: #3385ff;
-    --light: #fff;
-    --dark-purple: #4d0066;
-    --light-purple: #ac00e6; */
-    --shadow: 0 1px 5px rgba(104, 104, 104, 0.8);
-}
+<style scoped>
 
 </style>
