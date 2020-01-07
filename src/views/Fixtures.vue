abd55fc1282f2ef1151a31968f1e0d6ec81fc967
Rob Armitage <robert.c.armitage@gmail.com>
2020-01-07 23:59:30 +0000

    visible text on Fixtures and League Table pages

---
+++ src/views/Fixtures.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="fixtures">
+    <p>This is the Fixtures page</p>
+  </div>
+</template>
---
+++ src/views/LeagueTable.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="league_table">
+    <p>This is the League Table page</p>
+  </div>
+</template>
